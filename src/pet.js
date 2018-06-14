4162ae1e20cf7dca58cbc1932929d0b342dabee0
Daniel-Cross <crossy1686@gmail.com>
2018-06-14 23:15:02 +0100

    pet test code

---
+++ src/pet.js
@@ -0,0 +1,14 @@
+const Pet = require('../src/pet');
+
+function Pet(name) {
+  this.name = name;
+  this.age = 0;
+  this.growUp = function() {
+    this.age += 1;
+  };
+
+
+};
+
+module.exports = Pet;
+
