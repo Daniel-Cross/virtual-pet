ae96dea69882c9342d83da9c75af52b6aeb23072
Daniel-Cross <crossy1686@gmail.com>
2018-06-17 15:14:32 +0100

    completed up to step 9

--- src/pet.js
+++ src/pet.js
@@ -1,14 +1,97 @@
-const Pet = require('../src/pet');
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
+
 
 function Pet(name) {
   this.name = name;
   this.age = 0;
-  this.growUp = function() {
-    this.age += 1;
+  this.hunger = MINIMUM_HUNGER;
+  this.fitness = MAXIMUM_FITNESS;
+
+  }
+
+  Pet.prototype.isAlive = function() {
+
+    if(this.fitness === 0 && this.hunger === 10 && this.age === 30) {
+
+      return false;
+
+    } else {
+
+      return true;
+
+    }
+
+  };
+
+  Pet.prototype.isDead = function() {
+    if(pet.isAlive() === false) {
+
+      return false;
+
+    } else {
+
+      return true;
+
+    }
   };
 
 
-};
+  Pet.prototype.growUp = function() {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+  }
+
+  Pet.prototype.walk = function() {
+    if((this.fitness + 4) <= MAXIMUM_FITNESS) {
+        this.fitness += 4
+    } else {
+      this.fitness = MAXIMUM_FITNESS
+    }
+  };
+
+  Pet.prototype.feed = function() {
+    if((this.hunger - 3) >= MINIMUM_HUNGER) {
+      this.hunger -= 3
+    } else {
+      this.hunger = MINIMUM_HUNGER
+    }
+  };
+
+  Pet.prototype.checkUp = function() {
+
+    if(this.hunger >= 5 && this.fitness <= 3) {
+
+      return 'I am hungry AND I need a walk';
+
+    } else if(this.hunger >= 5) {
+
+      return 'I am hungry';
+
+    } else if(this.fitness <= 3) {
+
+      return 'I need a walk';
+
+    } else {
+
+      return 'I feel great!'
+
+    }
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = Pet;
-
